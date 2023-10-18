2890ba060abda1f044e955f023c1678db4126456
Brick <eskonikita@gmail.com>
2023-10-18 15:22:52 +0300

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './reset.css';
 import './app.css';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from "./components/header/Header";
 import Footer from './components/footer/Footer';
